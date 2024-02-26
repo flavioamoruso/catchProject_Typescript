ec8b4400eaf3f6a9044597b20e000cbbc16a7e36
FLAVIO AMORUSO <flavioamoruso6.com>
2024-02-26 14:10:18 +0100

    configurazione card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import { StyledCard } from "./styled.index";
+
+const Card: FunctionComponent = () => {
+  return <StyledCard></StyledCard>;
+};
+
+export default Card;
---
+++ src/components/Card/styled.index.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  width: 400px;
+  height: 600px;
+  display: flex;
+  border-radius: 16px;
+`;
+
+export { StyledCard };
